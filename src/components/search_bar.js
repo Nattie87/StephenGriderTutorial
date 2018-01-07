8d2142013686af717a2a21243fd1c73af9cb2780
Nattie87 <natalie.huitson@gmail.com>
2018-01-07 19:34:08 +0000

    updated the state of the search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component{
+  constructor(props) {
+    super(props); //super calls the parent method of the component 
+    this.state = { 
+    term: ''
+  };
+    
+  }
   render() {
-    return <input onChange={event => console.log(event.target.value)}/>;
+    return (
+      <div>
+    <input onChange={event => this.setState({ term: event.target.value })}/>
+    // Value of input is { this.state.term}
+    </div>
+  );
   }
 
 }
