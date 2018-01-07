bead1e82f6a1be1ebdb3ef4eba90ad9188f3e307
Nattie87 <natalie.huitson@gmail.com>
2018-01-07 20:16:41 +0000

    videos in state are a go go

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
-
 const API_KEY = 'AIzaSyCb7XA6857qpeQ64M82AehbMq26o-nFYxU';
 
-YTSearch({ key: API_KEY, term: 'beyonce' }, function(data) {
-  console.log(data);
-});
-
-
 //Creating a component that is going to produce some html.
-const App = () => {
-  return (
-    <div>
-    <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {videos: []};
+    
+    YTSearch({ key: API_KEY, term: 'beyonce' },(videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+      <SearchBar />
+      </div>
+    );
+  }
 }
 
 //I will take this components generated HTML and put it on the page...pop it in the DOM.
