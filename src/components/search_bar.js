9dd7c64c7979f2ad398dddf0b7ddc3d167e040f7
Nattie87 <natalie.huitson@gmail.com>
2018-01-14 16:07:12 +0000

    completed some basic styling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component{
   constructor(props) {
-    super(props); //super calls the parent method of the component 
-    this.state = { 
+    super(props); //super calls the parent method of the component
+    this.state = {
     term: ''
   };
-    
+
   }
   render() {
     return (
-      <div>
-    <input 
+      <div className="search-bar">
+    <input
     value={this.state.term}
     onChange={event => this.setState({ term: event.target.value })}/>
     </div>
