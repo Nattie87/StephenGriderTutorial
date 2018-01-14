0a07b7ab37233abe75b2e7818cb48906f23e41df
Nattie87 <natalie.huitson@gmail.com>
2018-01-14 13:10:27 +0000

    images and title rendering on page. Added url snippet

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,7 @@ import VideListItem from './video_list_item';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideListItem video={video} />
+    return <VideListItem key={video.etag} video={video} />
   });
   return (
     <ul className="col-md-4 list-group">
