7237a7c25ea54fa3f982931713e5f15dd2e6fd8f
Nattie87 <natalie.huitson@gmail.com>
2018-01-14 16:30:33 +0000

    searchbar picking up and rendering search words

--- src/index.js
+++ src/index.js
@@ -16,17 +16,23 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({ key: API_KEY, term: 'beyonce' },(videos) => {
+    this.videoSearch('Larry David');
+  }
+
+
+  videoSearch (term) {
+    YTSearch({ key: API_KEY, term: term },(videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
     });
   }
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
         onVideoSelect={selectedVideo => this.setState({selectedVideo})}
